55f71e403174658152bf4a48f57efd82bc44dfa0
Robin Pal <robin.11800016@lpu.in>
2020-06-29 15:36:54 +0530

    Create ReAddision.java
    addision of given number

---
+++ ReAddision.java
@@ -0,0 +1,16 @@
+
+public class ReAddision {
+    static int addision(int n) {
+        if (n < 10)
+            return n;
+
+        return addision(n / 10) + n % 10;
+        //System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        int n = 243;
+        int result =addision(n);
+        System.out.println(result);
+    }
+}

0f701ae4b9db332459576a7bdaf7bb65a88acb74
Robin Pal <robin.11800016@lpu.in>
2020-07-07 12:31:47 +0530

    Create Josepus.java

---
+++ Josepus.java
@@ -0,0 +1,14 @@
+
+public class Josepus {
+    static int jos(int n,int k){
+        if (n==1)
+        return 0;
+        else
+          return(jos(n-1,k)+k)%n;
+    }
+    public static void main(String[] args) {
+        int n=6;
+        int k=3;
+        System.out.println(jos(n,k));
+    }
+}
